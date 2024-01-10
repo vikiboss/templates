265fa6d2389a18310fb96a90e4a0ca495dfea807
Viki <hi@viki.moe>
2024-01-10 15:38:03 +0800

    feat: refine react template

--- templates/react-ts-vite-unocss/src/main.tsx
+++ templates/react-ts-vite-unocss/src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client'
+
+import App from './app.tsx'
+
+import 'virtual:uno.css'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const mainDiv = document.getElementById('app')
+
+if (mainDiv) {
+  const root = createRoot(mainDiv)
+
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+} else {
+  console.error('appDiv is null')
+}
