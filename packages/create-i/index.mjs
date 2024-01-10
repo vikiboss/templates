caf0bca7d2f4d17cd03de917b4b1bf33011bc2ac
Viki <hi@viki.moe>
2024-01-10 13:08:19 +0800

    feat: wip cli

--- packages/create-i/index.mjs
+++ packages/create-i/index.mjs
@@ -1,7 +1,51 @@
 #!/usr/bin/env node
 
+import fs from 'node:fs'
 import got from 'got'
+import degit from 'degit'
+import prompts from 'prompts'
+import path from 'node:path'
 
-console.log('loading templates...')
+const link = 'https://api.github.com/repos/vikiboss/templates/contents/templates'
 
-const templates = await got('https://api.github.com/users/lukeed/repos', {})
+console.log('⏳ loading templates...')
+
+/** @type {{ name: string }[]} */
+const templates = await got.get(link).json()
+
+console.log('✅ templates loaded')
+
+/** @type {{ template: string, name: string, overwrite: boolean }} */
+const { template, name, overwrite } = await prompts([
+  {
+    type: 'select',
+    name: 'template',
+    message: 'Select a template',
+    choices: templates.map(({ name }) => ({ title: name, value: name }))
+  },
+  {
+    type: 'text',
+    name: 'name',
+    message: 'Project name'
+  },
+  {
+    type: (_, { name }) => {
+      const dirExists = fs.existsSync(path.join(process.cwd(), name))
+      return name && dirExists ? 'confirm' : null
+    },
+    name: 'overwrite',
+    message: 'Overwrite existing files?'
+  }
+])
+
+const emitter = degit(`vikiboss/templates/templates/${template}`, {
+  cache: false,
+  verbose: true,
+  force: overwrite
+})
+
+console.log('⏳ cloning template...')
+
+await emitter.clone(name)
+
+console.log('✅ template cloned')
